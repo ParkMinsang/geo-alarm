a9de841ad6431083f43416040f1d7c718857b259
winter.min <winter.min@kakaomobility.com>
2025-08-03 15:23:00 +0900

    Initial Commit

---
+++ Geo-Alarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Geo-Alarm
+//
+//  Created by winter.min on 8/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Geo-Alarm/Geo_AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Geo_AlarmApp.swift
+//  Geo-Alarm
+//
+//  Created by winter.min on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Geo_AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
